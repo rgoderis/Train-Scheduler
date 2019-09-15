468134f3ad46130092c5a332308289f0bab867a0
Ryan Goderis <rgoderis789@gmail.com>
2019-09-15 15:01:11 -0400

    linked bootstrap, jquery, and firebase database

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyCWD1SAsbOxri31S5xHHHE-GGcRPtZMJsw",
+    authDomain: "trainscheduler-2e567.firebaseapp.com",
+    databaseURL: "https://trainscheduler-2e567.firebaseio.com",
+    projectId: "trainscheduler-2e567",
+    storageBucket: "",
+    messagingSenderId: "322959206384",
+    appId: "1:322959206384:web:6aa67b4086335381a5df35"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
