e7aedf6bb7aba67ef861b92a9a36e6f68e0c64f1
Ryan Goderis <rgoderis789@gmail.com>
2019-09-15 15:50:09 -0400

    set click listener for train-submit, created vars for train info

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,10 +9,30 @@
     appId: "1:322959206384:web:6aa67b4086335381a5df35"
   };
   // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+//   firebase.initializeApp(firebaseConfig);
+
+//   create var to reference database
+// var database = firebase.database();
+
+// variables for input info
+var trainName = "";
+var trainDestination = "";
+var trainTime = "";
+var trainFrequency = "";
+
+
+$(document).ready(function(){
+
+
+// set click listener to #train-submit
+$("#train-submit").on("click", function(){
+    alert("You clicked the button")
+});
 
 // save train information from input 
 
 // push saved train information to database
 
 // retrieve train information from database to display in train display div
+
+});
